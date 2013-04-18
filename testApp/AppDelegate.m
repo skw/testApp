f0705730c0ecb1f402a8af34870abce228d5b5c0
shaunwong <shaun.kirk.wong@gmail.com>
2013-04-18 16:02:16 -0700

    first commit

--- testApp/AppDelegate.m
+++ testApp/AppDelegate.m
@@ -7,14 +7,21 @@
 //
 
 #import "AppDelegate.h"
-
+#import "MainViewController.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    // create instance of MainViewController and set it as the root view controller
+    MainViewController *viewController = [[MainViewController alloc] init];
+    
+    self.window.rootViewController = viewController;
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ testApp/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  testApp
+//
+//  Created by ETCL Dev on 2013-04-17.
+//  Copyright (c) 2013 ETCL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController <UITextFieldDelegate>
+
+@end
